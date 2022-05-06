de1abc6b20e9621a5e9417f90ed8ee5bf21355da
shiori <chio1026.002@gmail.com>
2022-05-07 06:32:35 +0900

    jQuery追加

---
+++ js/script.js
@@ -0,0 +1,68 @@
+$(function () {
+  $(".l-header__menu").on("click", function () {
+    $(".c-cover").toggleClass("active");
+    $(".p-sidebar").toggleClass("active");
+    $("body").css("position", "fixed");
+  });
+  $(".c-close").on("click", function () {
+    $(".c-cover").toggleClass("active");
+    $(".p-sidebar").toggleClass("active");
+    $("body").css("position", "");
+  });
+  $(window).resize(function () {
+    let $window = $(this).width();
+    let breakpoint = 1025;
+
+    if ($window > breakpoint) {
+      $(".p-sidebar").show();
+      $(".c-cover").removeClass("active");
+      $("body").css("position", "");
+    } else {
+      $(".p-sidebar").removeClass("active");
+      $(".c-cover").removeClass("active");
+      $("body").css("position", "");
+    }
+  });
+});
+
+
+// $(function () {
+//   $(".l-header__menu").click(function () {
+//     $(this).toggleClass("active");
+//     $(".c-cover").fadeToggle();
+//     $(".l-sidebar").toggleClass("open");
+//     $("body").toggleClass("open");
+//   })
+//   $(".p-sidebar__close").click(function () {
+//     $("c-cover").fadeOut();
+//     $(".l-header__menu").removeClass("active");
+//     $("body").removeClass("open");
+//   });
+// })
+
+// jQuery(function ($) {
+//   $(".l-header__menu").on("click", function () {
+//     $(".c-cover").toggleClass("active");
+//     $(".p-sidebar").toggleClass("active");
+//     $("body").css("position", "fixed");
+//   });
+//   $(".c-close").on("click", function () {
+//     $(".c-cover").toggleClass("active");
+//     $(".p-sidebar").toggleClass("active");
+//     $("body").css("position", "");
+//   });
+//   $(window).resize(function () {
+//     let $window = $(this).width();
+//     let breakpoint = 1025;
+
+//     if ($window > breakpoint) {
+//       $(".p-sidebar").show();
+//       $(".c-cover").removeClass("active");
+//       $("body").css("position", "");
+//     } else {
+//       $(".p-sidebar").removeClass("active");
+//       $(".c-cover").removeClass("active");
+//       $("body").css("position", "");
+//     }
+//   });
+// });
